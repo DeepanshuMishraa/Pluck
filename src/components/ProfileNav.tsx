4e74bca7d73617ff53957d67c1038291a4877eeb
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-03-03 23:14:32 +0530

    toast&home done fix

--- src/components/ProfileNav.tsx
+++ src/components/ProfileNav.tsx
@@ -1,12 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import React from "react";
-import { FaAlignLeft, FaAlignRight } from "react-icons/fa";
-import {FaBars,FaTimes} from 'react-icons/fa'
+
 
 const Navbar = () => {
-    const [nav,setNav] = React.useState(false);
 
   return (
     <div className="md:w-[16rem] bg-inherit h-16 flex relative md:left-[34rem] md:p-2 md:mt-1 rounded-full md:text-white md:space-x-4 p-2 border-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] mt-4 w-[14rem] left-[7rem]  ">
