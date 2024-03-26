c25099d58482cf2f1fb1eec4dded654ee9260f1d
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-03-26 22:37:56 +0530

    Update route.ts

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -44,7 +44,7 @@ export async function POST(request: NextRequest) {
 
     //send email verfication
 
-    await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
+  
 
     return NextResponse.json({
       message: "User Created Succesfully",
